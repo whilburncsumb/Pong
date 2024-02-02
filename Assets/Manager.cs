9391aadcdcccecdad0a74beaca5397d09a4391ab
William Hilburn <123123775+whilburncsumb@users.noreply.github.com>
2024-02-01 23:18:41 -0800

    added notes

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -31,3 +31,46 @@ public class Manager : MonoBehaviour
         ball.StartRound();
     }
 }
+
+/*
+ Instructions: 
+https://docs.google.com/document/d/1zvTCqMXNgnaEEgkUyic_q291N7PWWan3RHkbNqSYLak/edit
+ 
+https://docs.unity3d.com/Manual/collider-types-interaction.html
+
+Score
+- when balls goes past paddle, player on opposite side increase score by 1
+- max score 99?
+
+Sound
+- ball collides with something
+	- different for paddle or wall
+
+Presentation
+- 2D
+- blocky
+- minimal color (black, white)
+- middle line (visual only)
+- top and bottom lines confine ball as a boundary
+
+Ball
+- speeds up after bounce
+- angle off of paddle is not real-world physics
+- "ball" is square
+- spawns in the middle toward the previous scoring player's side
+- ball returns to original starting velocity upon spawn
+
+Paddle
+- moves vertically
+- constrained by top and bottom boundaries
+- constant velocity/speed
+
+Players
+- single player or 2 player
+
+Goal / Game Rules
+- ba
+
+Dynamics
+- speed of paddles makes predicting trajectories necessary to win / not lose
+*/
